a1d32ba78f47eb356230f525f27842d702e36424
David Begin <davidmichaelbe@gmail.com>
2015-11-10 22:11:40 -0800

    Refactor main class and help stop rate limiting

--- lib/slack_bomb.rb
+++ lib/slack_bomb.rb
@@ -1,70 +1,94 @@
 require "slack_bomb/version"
+require 'optparse'
 require 'faker'
 
 module SlackBomb
-	class << self
+  API_BUFFER = 0.02
 
-		def bomb!
-			loop do
-				[
-					:neckbeard,
-					:no_good,
-					:hurtrealbad,
-					:whale,
-					:space_invader,
-					:horse_racing,
-					:bowtie,
-					:stuck_out_tongue_winking_eye,
-					:goberserk,
-					:squirrel,
-					:octopus,
-					:sun_with_face,
-					:boar,
-					:eyes,
-					:dragon_face,
-					:neutral_face,
-					:shit,
-					:smiling_imp,
-					:lips,
-					:koala,
-					:girl,
-					:muscle,
-					:dizzy_face,
-					:bride_with_veil,
-					:pray,
-					:man,
-					:speak_no_evil,
-					:trollface,
-					:ghost,
-					:dolls,
-					:octocat,
-					:fish,
-					:cop,
-					:flushed,
-					:rocket,
-					:honey_pot,
-					:panda_face
-				].shuffle.each do |key|
-					SlackBomb.post_to_slack(key)
-				end
-			end
-		end
+  class << self
+    def bomb!
+      threads = (1..5).to_a.map do
+        EMOJIS.shuffle.map do |key|
+          Thread.new do
+            SlackBomb.post_to_slack(key)
+          end
+        end
+      end.flatten
+      threads.each(&:join)
+    end
 
-		def channel
-			"#bot-island"
-		end
+    def post_to_slack(key)
+      system full_curl(key)
+      sleep API_BUFFER
+    end
 
-		def post_to_slack(key)
-			`curl -X POST --data-urlencode 'payload={"channel": "#{channel}", "username": "#{name}", "text": "#{Faker::Company.catch_phrase} is the right solution!", "icon_emoji": ":#{key}:"}' #{slack_hook}`
-		end
+    private
 
-		def channels
-			["#development"]
-		end
+    def channel
+      "#bot-island"
+    end
 
-		def slack_hook
-			"https://hooks.slack.com/services/SOMERANDOMLETERSANDNUMBERSFORYOURSLACKHOOK"
-		end
-	end
+    def name
+      Faker::Name.name
+    end
+
+    def full_curl(key)
+      <<-CURL
+        #{curl_cmd}\
+        'payload={\
+          "channel": "#{channel}",\
+          "username": "#{name}",\
+          "text": "#{Faker::Company.catch_phrase} is the right solution!",\
+          "icon_emoji": ":#{key}:"}' #{slack_hook}
+      CURL
+    end
+
+    def curl_cmd
+      "curl -X POST --data-urlencode"
+    end
+
+    def slack_hook
+      "https://hooks.slack.com/services/YOUR/SLACK/API_KEY"
+    end
+  end
+
+  EMOJIS = [
+    :neckbeard,
+    :no_good,
+    :hurtrealbad,
+    :whale,
+    :space_invader,
+    :horse_racing,
+    :bowtie,
+    :stuck_out_tongue_winking_eye,
+    :goberserk,
+    :squirrel,
+    :octopus,
+    :sun_with_face,
+    :boar,
+    :eyes,
+    :dragon_face,
+    :neutral_face,
+    :shit,
+    :smiling_imp,
+    :lips,
+    :koala,
+    :girl,
+    :muscle,
+    :dizzy_face,
+    :bride_with_veil,
+    :pray,
+    :man,
+    :speak_no_evil,
+    :trollface,
+    :ghost,
+    :dolls,
+    :octocat,
+    :fish,
+    :cop,
+    :flushed,
+    :rocket,
+    :honey_pot,
+    :panda_face
+  ]
 end
-
